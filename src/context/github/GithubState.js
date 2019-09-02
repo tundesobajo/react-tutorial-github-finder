6d3feb7763b9e6e7100147ff5ea41d01b0c75554
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2019-09-02 13:26:01 +0100

    Created context for application state

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,65 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import { GET_USER, GET_USERS, SEARCH_USERS, CLEAR_USERS, GET_REPOS, SET_LOADING } from '../types';
+import usersData from '../../data/UsersData';
+
+const secret = `client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+
+const GithubState = props => {
+  const initialState = {
+    user: {},
+    users: [],
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Get Single github user
+  const getUser = async username => {
+    setLoading();
+    const res = await axios.get(`https://api.github.com/users/${username}?${secret}`);
+    dispatch({ type: GET_USER, payload: res.data });
+  };
+  // Load default user list page
+  const getUsers = async () => {
+    setLoading();
+    // const res = await axios.get(`https://api.github.com/users?${secret}`);
+    // dispatch({ type: SEARCH_USERS, payload: res.data.items });
+    dispatch({ type: GET_USERS, payload: usersData.data });
+  };
+  // Get user by search name
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&${secret}`);
+    dispatch({ type: SEARCH_USERS, payload: res.data.items });
+  };
+  // Clear users
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
+  // Get github user repos
+  const getUserRepos = async username => {
+    setLoading();
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&${secret}`);
+    dispatch({ type: GET_REPOS, payload: res.data });
+  };
+  // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  return <GithubContext.Provider value={{
+    user: state.user,
+    users: state.users,
+    repos: state.repos,
+    loading: state.loading,
+    searchUsers,
+    getUser,
+    getUsers,
+    clearUsers,
+    getUserRepos,
+  }}>
+    {props.children}
+  </GithubContext.Provider>;
+};
+
+export default GithubState;
